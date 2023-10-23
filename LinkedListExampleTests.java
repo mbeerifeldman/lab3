355aa17cf7287b15858ed2981d7011dffd2865fc
mbeerifeldman <114952150+mbeerifeldman@users.noreply.github.com>
2023-10-23 15:45:50 -0700

    Create LinkedListExampleTests.java

---
+++ LinkedListExampleTests.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.List;
+import java.util.ArrayList;
+
+public class LinkedListExampleTests {
+    ListChecker sc = new ListChecker();
+	@Test 
+	public void testPrepend() {
+        LinkedList.prepend(value)
+        
+        assertEquals(input2, );
+	}
+}
